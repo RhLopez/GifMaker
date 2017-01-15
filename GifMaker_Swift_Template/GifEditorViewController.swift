67529c551391ce7799d2d9c8b235d40716dd37ce
Ramiro H. Lopez <RhL@ramiros-imac.fios-router.home>
2017-01-14 22:55:34 -0800

    initial commit

---
+++ GifMaker_Swift_Template/GifEditorViewController.swift
@@ -0,0 +1,9 @@
+//
+//  GifEditorViewController.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Ramiro H. Lopez on 1/14/17.
+//  Copyright © 2017 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
