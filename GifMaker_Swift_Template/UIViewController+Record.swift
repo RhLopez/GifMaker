ae8ddeb2340247985eb72a2d04bff4830d928b9e
Ramiro H. Lopez <RhL@ramiros-imac.fios-router.home>
2017-01-15 15:46:34 -0800

    add: UIViewController extension

---
+++ GifMaker_Swift_Template/UIViewController+Record.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController+Record.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Ramiro H. Lopez on 1/15/17.
+//  Copyright © 2017 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MobileCoreServices
+
+extension UIViewController {
+    
+}
