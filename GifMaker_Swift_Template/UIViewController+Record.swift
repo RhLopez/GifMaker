a218f75f11af85b331fdb6a3c94e1cac7c316777
Ramiro H. Lopez <RhL@ramiros-imac.fios-router.home>
2017-01-15 16:03:26 -0800

    add: imagePickerControllerDelegate functions

--- GifMaker_Swift_Template/UIViewController+Record.swift
+++ GifMaker_Swift_Template/UIViewController+Record.swift
@@ -10,8 +10,7 @@ import Foundation
 import UIKit
 import MobileCoreServices
 
-extension UIViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
+extension UIViewController {
     @IBAction func launchVideoCamera(sender: AnyObject) {
         let recordVideoController = UIImagePickerController()
         recordVideoController.sourceType = .camera
@@ -19,5 +18,23 @@ extension UIViewController: UIImagePickerControllerDelegate, UINavigationControl
         recordVideoController.delegate = self
         
         present(recordVideoController, animated: true, completion: nil)
+    }
+}
+
+extension UIViewController: UINavigationControllerDelegate {}
+
+extension UIViewController: UIImagePickerControllerDelegate {
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let mediaType = info[UIImagePickerControllerMediaType] as! String
+        
+        if mediaType == kUTTypeMovie as String {
+            let videoURL = info[UIImagePickerControllerMediaURL] as! NSURL
+            UISaveVideoAtPathToSavedPhotosAlbum(videoURL.path!, nil, nil, nil)
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 }
