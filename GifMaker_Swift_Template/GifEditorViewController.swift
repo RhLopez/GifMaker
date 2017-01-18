f97a8e8846ad140a455ece6b530c6cca63494126
Ramiro H. Lopez <RhL@ramiros-imac.fios-router.home>
2017-01-17 22:55:05 -0800

    add: keyboardNotification

--- GifMaker_Swift_Template/GifEditorViewController.swift
+++ GifMaker_Swift_Template/GifEditorViewController.swift
@@ -17,6 +17,7 @@ class GifEditorViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         gifImageView.image = gif?.gifImage
+        subscribeToKeyboardNotification()
     }
     
     override func viewDidLoad() {
@@ -33,5 +34,40 @@ extension GifEditorViewController: UITextFieldDelegate {
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
         textField.placeholder = ""
+    }
+}
+
+extension GifEditorViewController {
+    func subscribeToKeyboardNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotification() {
+        NotificationCenter.default.removeObserver(NSNotification.Name.UIKeyboardWillShow)
+        NotificationCenter.default.removeObserver(NSNotification.Name.UIKeyboardWillHide)
+    }
+    
+    func keyboardWillShow(notification: Notification) {
+        if (self.view.frame.origin.y >= 0) {
+            var rect: CGRect = self.view.frame
+            rect.origin.y  -= getKeyboardHeight(notifcation: notification)
+            self.view.frame = rect
+        }
+    }
+    
+    func keyboardWillHide(notification: Notification) {
+        if (self.view.frame.origin.y < 0) {
+            var rect: CGRect = self.view.frame
+            rect.origin.y += getKeyboardHeight(notifcation: notification)
+            self.view.frame = rect
+        }
+    }
+    
+    func getKeyboardHeight(notifcation: Notification) -> CGFloat {
+        let userInfo: Dictionary = notifcation.userInfo!
+        let keyBoardSize = userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue
+        
+        return keyBoardSize.cgRectValue.height
     }
 }
