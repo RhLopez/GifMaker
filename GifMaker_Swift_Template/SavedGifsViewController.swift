12de814ca7d0369170e8d174994309daf4c2d1f3
RhLopez <ramiro.h.lopez@outlook.com>
2017-01-25 23:34:27 -0800

    add: CollectionView dataSource, delegate, flowLayout

--- GifMaker_Swift_Template/SavedGifsViewController.swift
+++ GifMaker_Swift_Template/SavedGifsViewController.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2017 Gabrielle Miller-Messner. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SavedGifsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    var savedGifs = [Gif]()
+    let cellMargin: CGFloat = 12.0
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+//        return savedGifs.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GifCell", for: indexPath) as! GifCell
+//        let gif = savedGifs[indexPath.item]
+//        cell.configureForGif(gif: gif)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.frame.size.width - (cellMargin * 2.0)) / 2.0
+        return CGSize(width: width, height: width)
+    }
+}
